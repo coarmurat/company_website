45b9cab12deb76856372235ef4bdef3ae82b2438
Murat Coşar <coarmurat@gmail.com>
2023-11-09 14:03:26 +0300

    reviews

--- src/app/(Landing Page)/_components/MusteriYorumları/index.tsx
+++ src/app/(Landing Page)/_components/MusteriYorumları/index.tsx
@@ -1,11 +1,149 @@
 import styles from './styles.module.css'
-import underLİne from '@/styles/underLine.module.css'
+import underLine from '@/styles/underLine.module.css'
 
 export default function MusteriYorumlari() {
     
     return(
         <section className={styles.musteriYorumlari}>
-            <h2 className={underLİne.className}>Müşteri Yorumları</h2>
+            <h2 className={underLine.className}>Müşteri Yorumları</h2>
+            <div className={styles.customers}>
+                <div className={styles.left}></div>
+                <div className={styles.profiles}>
+                    <div className={styles.profile}>
+                        <div className={styles.photo}></div>
+                        <div className={styles.name}>Abc D.</div>
+                        <div className={styles.stars}>
+                            <div className={styles.pointed}></div>
+                            <div className={styles.pointed}></div>
+                            <div className={styles.pointed}></div>
+                            <div className={styles.pointed}></div>
+                            <div></div>
+                        </div>
+                    </div>
+                    <div className={`${styles.profile} ${styles.middleProfile}`}>
+                        <div className={styles.photo}></div>
+                        <div className={styles.name}>Abc D.</div>
+                        <div className={styles.stars}>
+                            <div className={styles.pointed}></div>
+                            <div className={styles.pointed}></div>
+                            <div className={styles.pointed}></div>
+                            <div className={styles.pointed}></div>
+                            <div></div>
+                        </div>
+                    </div>
+                    <div className={styles.profile}>
+                        <div className={styles.photo}></div>
+                        <div className={styles.name}>Abc D.</div>
+                        <div className={styles.stars}>
+                            <div className={styles.pointed}></div>
+                            <div className={styles.pointed}></div>
+                            <div className={styles.pointed}></div>
+                            <div className={styles.pointed}></div>
+                            <div></div>
+                        </div>
+                    </div>
+                    <div className={styles.profile}>
+                        <div className={styles.photo}></div>
+                        <div className={styles.name}>Abc D.</div>
+                        <div className={styles.stars}>
+                            <div className={styles.pointed}></div>
+                            <div className={styles.pointed}></div>
+                            <div className={styles.pointed}></div>
+                            <div className={styles.pointed}></div>
+                            <div></div>
+                        </div>
+                    </div>
+                    <div className={styles.profile}>
+                        <div className={styles.photo}></div>
+                        <div className={styles.name}>Abc D.</div>
+                        <div className={styles.stars}>
+                            <div className={styles.pointed}></div>
+                            <div className={styles.pointed}></div>
+                            <div className={styles.pointed}></div>
+                            <div className={styles.pointed}></div>
+                            <div></div>
+                        </div>
+                    </div>
+                    <div className={styles.profile}>
+                        <div className={styles.photo}></div>
+                        <div className={styles.name}>Abc D.</div>
+                        <div className={styles.stars}>
+                            <div className={styles.pointed}></div>
+                            <div className={styles.pointed}></div>
+                            <div className={styles.pointed}></div>
+                            <div className={styles.pointed}></div>
+                            <div></div>
+                        </div>
+                    </div>
+                    <div className={styles.profile}>
+                        <div className={styles.photo}></div>
+                        <div className={styles.name}>Abc D.</div>
+                        <div className={styles.stars}>
+                            <div className={styles.pointed}></div>
+                            <div className={styles.pointed}></div>
+                            <div className={styles.pointed}></div>
+                            <div className={styles.pointed}></div>
+                            <div></div>
+                        </div>
+                    </div>
+                    <div className={styles.profile}>
+                        <div className={styles.photo}></div>
+                        <div className={styles.name}>Abc D.</div>
+                        <div className={styles.stars}>
+                            <div className={styles.pointed}></div>
+                            <div className={styles.pointed}></div>
+                            <div className={styles.pointed}></div>
+                            <div className={styles.pointed}></div>
+                            <div></div>
+                        </div>
+                    </div>
+                    <div className={styles.profile}>
+                        <div className={styles.photo}></div>
+                        <div className={styles.name}>Abc D.</div>
+                        <div className={styles.stars}>
+                            <div className={styles.pointed}></div>
+                            <div className={styles.pointed}></div>
+                            <div className={styles.pointed}></div>
+                            <div className={styles.pointed}></div>
+                            <div></div>
+                        </div>
+                    </div>
+                    <div className={styles.profile}>
+                        <div className={styles.photo}></div>
+                        <div className={styles.name}>Abc D.</div>
+                        <div className={styles.stars}>
+                            <div className={styles.pointed}></div>
+                            <div className={styles.pointed}></div>
+                            <div className={styles.pointed}></div>
+                            <div className={styles.pointed}></div>
+                            <div></div>
+                        </div>
+                    </div>
+                    <div className={styles.profile}>
+                        <div className={styles.photo}></div>
+                        <div className={styles.name}>Abc D.</div>
+                        <div className={styles.stars}>
+                            <div className={styles.pointed}></div>
+                            <div className={styles.pointed}></div>
+                            <div className={styles.pointed}></div>
+                            <div className={styles.pointed}></div>
+                            <div></div>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.right}></div>
+            </div>
+            <div className={styles.reviews}>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab cumque dignissimos sequi voluptatum eligendi! Quidem laudantium, possimus blanditiis recusandae, exercitationem, dolore ullam fugiat asperiores obcaecati vitae dolores provident explicabo incidunt.
+                </p>
+                <p>
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime optio voluptates doloremque ratione, asperiores minus atque quidem fugit nulla deserunt voluptas ex recusandae dolor, autem id ab? Quisquam, tenetur cum.
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt quos officiis beatae recusandae, quia ipsum repudiandae corrupti. Maxime alias excepturi ratione modi, adipisci beatae hic repudiandae laboriosam aut voluptatem nemo.
+                </p>
+            </div>
         </section>
     )
 }
